decfb8de4f85e50b358db913b2ccd49011f2ed05
MekhiHart <mekhihartdelacruz@gmail.com>
2023-01-24 21:38:31 -0800

    Added a single table for the Schedule compoenent

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -1,5 +1,16 @@
 export default function Schedule(props){
     return (
-        <h1>This is the Schedule component</h1>
+        <div className="schedule--table">
+            <div className="schedule--div">
+                <h3>00:00 AM</h3>
+                <h3> <b>Python Competition </b> Coding Competetion</h3>
+                <p>Show off your python Skills in a collaborative project. Up to 5 team members using the python programming language</p>
+                <div className="schedule--location">Room 432</div>
+            </div>
+
+            {/* <div className="schedule--div">
+
+            </div> */}
+        </div>
     )
 }
