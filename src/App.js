452ce676de77669b833a3e3892ac9a81111bdbf1
MekhiHart <mekhihartdelacruz@gmail.com>
2023-01-30 15:46:06 -0800

    Made schedule responsive, and made changes on how it looks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-
-import Schedule from "./Components/Schedule"
+import Schedule from './Components/Schedule';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h1>Schedule Alerts</h1>
-
       <Schedule/>
-      
 
       </header>
     </div>
--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -1,16 +1,37 @@
 export default function Schedule(props){
-    return (
+    const generateEvents = () =>{
+        const numEvents = 5
+        const sampleData = []
+        const sampleEvent = {
+            time: "00:00AM",
+            event:{
+                title:"Python Competetion",
+                category: "Coding Competetion"
+            },
+            description: "Show off your python Skills in a collaborative project. Up to 5 team members using the python programming language",
+            location: "Room 402"
+        }
+    
+        for (let i=0;i<numEvents; i++){
+            sampleData.push(sampleEvent)
+        }
+    
+        return sampleData
+    }
+    
+    const sampleEvents = generateEvents().map(table => (
         <div className="schedule--table">
-            <div className="schedule--div">
-                <h3>00:00 AM</h3>
-                <h3> <b>Python Competition </b> Coding Competetion</h3>
-                <p>Show off your python Skills in a collaborative project. Up to 5 team members using the python programming language</p>
-                <div className="schedule--location">Room 432</div>
+            <div className="schedule--event">
+                <div className="schedule--event--time">{table.time}</div>
+                <div className="schedule--event--title"><b>{table.event.title}</b> <br/> {table.event.category}</div>
+                <div className="schedule--event--description">{table.description} </div>
+                <div className="schedule--event--location"> <h2>{table.location}</h2></div>
             </div>
+        </div> 
+    ))
 
-            {/* <div className="schedule--div">
-
-            </div> */}
-        </div>
+    
+    return (
+        sampleEvents
     )
-}
+  }
