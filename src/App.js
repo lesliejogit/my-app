c6d4b8328198214c71a4ac47d885bcbbe091cde4
Ricearoni26 <joey.rice01@student.csulb.edu>
2023-01-30 14:27:42 -0800

    Adding Google Calendar Button
    Adding the custom google calendar button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Schedule Alerts</h1>
-      
-
+        <h1>Schedule Alerts</h1>
+        <button class="Google-calendar-button">
+          <a target="_blank" rel="noopener noreferrer" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=MWduaThlY3JjcGVnZWxpcTV1dnR0cTYzaG0gMmI2MGM1MDgzMjk1ZGRkYzIwNWZhMTE4ODg0NTFlNmUxNjU2ZjkzNmU0Y2VmNjhkNDY5YWU2OGZiOGI5ZmU2YUBn&amp;tmsrc=2b60c5083295dddc205fa11888451e6e1656f936e4cef68d469ae68fb8b9fe6a%40group.calendar.google.com" style={{textDecoration: 'none', color: 'white'}}>
+            Add to Google Calendar
+          </a>
+        </button>
       </header>
     </div>
   );
