d5fb08c2b138316d390f60e85b77c7168e708a67
carinegordillo <64708532+carinegordillo@users.noreply.github.com>
2023-01-30 21:58:46 -0800

    Update Schedule.js

--- src/Components/Schedule.js
+++ src/Components/Schedule.js
@@ -1,23 +1,3 @@
-// export default function Schedule(props){ // ! prior attempt
-//     return (
-//         <div className="schedule--table">
-//             <div className="schedule--div">
-//                 <p>00:00 AM</p>
-//                 <p className="schedule--title--div"> <b>Python Competition </b> <br> Coding Competetion</p> 
-//                 <p className="schedule--description">Show off your python Skills in a collaborative project. Up to 5 team members using the python programming language</p>
-//                 <p className="schedule--location">Room 432</p>
-//             </div>
-  
-//             {/* <div className="schedule--div">
-  
-//             </div> */}
-//         </div>
-//     )
-//   }
-
-
-
-
 export default function Schedule(props){
     const generateEvents = () =>{
         const numEvents = 5
